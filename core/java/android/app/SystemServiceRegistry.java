816035e6dbf1fa0de9e8f4c883198037be343b47
Erfan Abdi <erfangplus@gmail.com>
2022-07-21 18:02:27 +0430

    base: Introduce LiveDisplay from Lineage
    Change-Id: Ie3a61a67edfc4f4568fa2341791fa9fa5263f90d
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -280,6 +280,10 @@ import com.android.internal.os.IDropBoxManagerService;
 import com.android.internal.policy.PhoneLayoutInflater;
 import com.android.internal.util.Preconditions;
 
+import com.android.internal.halcyon.app.LineageContextConstants;
+import com.android.internal.halcyon.hardware.LiveDisplayManager;
+import com.android.internal.halcyon.hardware.ILiveDisplayService;
+
 import java.util.Map;
 import java.util.Objects;
 
@@ -1040,6 +1044,19 @@ public final class SystemServiceRegistry {
                         final IAuthService service =
                                 IAuthService.Stub.asInterface(binder);
                         return new BiometricManager(ctx.getOuterContext(), service);
+                    }
+                });
+
+        registerService(LineageContextConstants.LINEAGE_LIVEDISPLAY_SERVICE, LiveDisplayManager.class,
+                new CachedServiceFetcher<LiveDisplayManager>() {
+                    @Override
+                    public LiveDisplayManager createService(ContextImpl ctx)
+                            throws ServiceNotFoundException {
+                        final IBinder binder =
+                                ServiceManager.getServiceOrThrow(LineageContextConstants.LINEAGE_LIVEDISPLAY_SERVICE);
+                        final ILiveDisplayService service =
+                                ILiveDisplayService.Stub.asInterface(binder);
+                        return new LiveDisplayManager(service);
                     }
                 });
 
