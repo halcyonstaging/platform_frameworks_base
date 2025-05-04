157182ea9741f6db09bf7f674db43ba28ec0e9a3
HeroBuxx <me@herobuxx.me>
2025-05-04 23:58:58 +0800

    core: Add setting to disable progress bar chip
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -19740,6 +19740,13 @@ public final class Settings {
         public static final String WINDOW_IGNORE_SECURE = "window_ignore_secure";
 
         /**
+         * Control whether to spoof current status of storage encryption.
+         * @hide
+         */
+        @Readable
+        public static final String SHOW_PROGRESS_BAR_CHIP = "show_progress_bar_chip";
+
+        /**
          * Setting indicating whether Low Power Standby is enabled, if supported.
          *
          * Values are:
